cec1601cabe5050bb92e9067a24ab606439ee8d2
unknown <lstradiot15@gmail.com>
2024-01-22 15:49:39 -0300

    Added rooutes

--- app/Http/Requests/Login.php
+++ app/Http/Requests/Login.php
@@ -18,8 +18,8 @@ class Login extends FormRequest
         return [
             'email' => [
                 'required',
-                'regex:/^[a-z0-9._%+-]+@[a-z.-]+\.(com\.ar|com)$/',
-                'unique:super_admins,email',
+                'email',
+                'regex:/^[a-z0-9._%+-]+@[a-z.-]+\.(com\.ar|com)$/'
             ],
             'password' => ['required', 'regex:/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$/']
         ];
@@ -29,9 +29,9 @@ class Login extends FormRequest
     public function messages()
     {
         return [
-            'email.unique' => 'El email ya se encuentra registrado.',
-            'email.required' => 'El email es obligatorio.',
-            'email.regex' => 'El formato del email debe ser nombre@dominio.com o .com.ar.',
+            'email.required' => 'El correo electrónico es obligatorio.',
+            'email.email' => 'Ingresa una dirección de correo electrónico válida.',
+            'email.regex' => 'El correo electrónico ingresado no respeta el formato correcto.',
             'password.required' => 'La contraseña es obligatoria.',
             'password.regex' => 'La contraseña debe tener al menos 8 caracteres, una letra mayúscula, minúscula y un número.',
         ];
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\Authentication;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/logout', [Authentication::class, 'logout']);
 });
+
+Route::post('/signup', [Authentication::class, 'signupSuperAdmin']);
+Route::post('/login', [Authentication::class, 'login']);
+Route::post('/password/forgot', [Authentication::class, 'forgotPassword']);
+Route::post('/password/reset', [Authentication::class, 'resetPassword']);
+Route::get('/verify-token/{token}', [Authentication::class, 'verifyResetToken']);
